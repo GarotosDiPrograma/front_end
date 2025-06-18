53fe0479c0e4eab1bd022108cb6c580ffa378421
Evileudo Matias <evileudo@gmail.com>
2025-06-18 16:52:54 -0300

    feat/pagina de pedidos

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,10 +1,10 @@
-
-import './Main.css'
-import './Produtos.css'
+import { Link } from 'react-router-dom';
+import './Produtos.css';
+import './Main.css';
 
 export const Produtos = () => {
   return (
-    <div className="produtos-container">
+    <div className="main-container produtos-container">
       <aside className="filtros">
         <h3>Filtrar por</h3>
 
@@ -52,13 +52,24 @@ export const Produtos = () => {
         </div>
 
         <div className="produtos-grid">
-          {[...Array(15)].map((_, i) => (
+          {[...Array(16)].map((_, i) => (
             <div className="produto-card" key={i}>
-              <span className="desconto">50% OFF</span>
-              <img src="/../../../public/K-SWISS.svg" alt="Tênis" />
-              <p className="categoria">Tênis</p>
-              <h3>K-Swiss V8 - Masculino</h3>
-              <p><s>R$ 200,00</s> <strong>R$ 100,00</strong></p>
+              {}
+              {i < 2 && <span className="desconto">50% OFF</span>}
+              
+              {}
+              <Link to={`/produto/${i + 1}`} className="produto-link">
+               <img src="/K-SWISS.svg" alt="Tênis"/>
+                <p className="categoria">Tênis</p>
+                <h3>K-Swiss V8 - Masculino</h3>
+                
+                {}
+                {i < 2 ? (
+                  <p><s>R$ 200,00</s> <strong>R$ 100,00</strong></p>
+                ) : (
+                  <p><strong>R$ 200,00</strong></p>
+                )}
+              </Link>
             </div>
           ))}
         </div>
